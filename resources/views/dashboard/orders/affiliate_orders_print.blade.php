7b0aee1ce9395096d2cd37e593b1408cddbaaf0c
unknown <islam.shaaban13@gmail.com>
2022-03-30 18:14:07 +0200

    fix affiliate commission

--- resources/views/dashboard/orders/affiliate_orders_print.blade.php
+++ resources/views/dashboard/orders/affiliate_orders_print.blade.php
@@ -144,7 +144,7 @@
                                     <td>{{ $product->pivot->stock }}</td>
                                     <td>{{ $product->min_price . ' ' . $order->user->country->currency }}</td>
                                     @if (!auth()->user()->HasRole('affiliate'))
-                                        <td>{{ $product->pivot->commission . ' ' . $order->user->country->currency }}
+                                        <td>{{ ($product->pivot->price - $product->min_price) * $product->pivot->stock .' ' .$order->user->country->currency }}
                                         </td>
                                     @endif
                                     <td>{{ $product->pivot->price * $product->pivot->stock . ' ' . $order->user->country->currency }}
