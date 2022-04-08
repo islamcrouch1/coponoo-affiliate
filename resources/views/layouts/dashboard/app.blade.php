1cd66dec306ebe5cd00491344f66548a1a513ab8
unknown <islam.shaaban13@gmail.com>
2022-04-08 21:21:56 +0200

    change site title

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Copono Dashboard</title>
+    <title>Sonoo - Dashboard</title>
 
     <link rel="icon" href="{{ asset('storage/img/fevicon.png') }}">
 
